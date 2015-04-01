fedb58646588b51efbbdf0fa89cdc6c7575bb65f
별님 <w7dn1ng75r@gmail.com>
2015-04-02 05:58:41 +0900

    v1.0.5

--- mozrepl/exception.py
+++ mozrepl/exception.py
@@ -11,6 +11,10 @@ class Exception(exceptions.Exception):
 	
 	javascript Error 객체를 참조 하듯이 속성을 참조하십시오. 단, 메소드에 대한 참조는 지원하지 않습니다.
 	
+	만약, 현재 객체에 존재하고 있는 이름과 같은 이름의 javascript object 속성에 접근하려면, __getitem__ 메소드를 사용하십시오.
+	
+	특정 속성이 존재하는가를 검사하려면 __contains__ 메소드를 사용하십시오.
+	
 	.. todo:: typeName, details 속성을 2.x 버전대에서 제거 할 것.
 	"""
 	def __init__(self, error):
@@ -18,9 +22,20 @@ class Exception(exceptions.Exception):
 		self.typeName = self._error.get('name', '')
 		self.details = ''
 	
+	def __contains__(self, key):
+		if key in self._error:
+			return True
+		return False
+	
 	def __getattr__(self, key):
 		if key in self._error:
 			return self._error[key]
+		#raise AttributeError('"{key}"는 존재하지 않는 키입니다.'.format(key=key))
+	
+	def __getitem__(self, key):
+		if key in self._error:
+			return self._error[key]
+		#raise AttributeError('"{key}"는 존재하지 않는 키입니다.'.format(key=key))
 	
 	def __str__(self):
 		msg = unicode()
